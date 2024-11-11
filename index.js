ef3754a206d3fed502cd78369331a48a487d0198
ljgthub <118673491+ljgthub@users.noreply.github.com>
2024-11-11 22:14:42 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+new_btn.addEventListener("click", function(){
+    
+    
+    if (item_form.className == "item_form_show") {item_form.className = "item_form"}
+    else {item_form.className = "item_form_show"}
+    
+})
+
+cancel_btn.addEventListener("click", function(){
+    
+    
+    if (item_form.className == "item_form_show") {item_form.className = "item_form"}
+    
+})
+
+
